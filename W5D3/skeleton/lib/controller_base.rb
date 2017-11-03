17a018796ef65c2746e0e1374e6a778c928d4e9a
David Bodow <dbodow@gmail.com>
2017-11-02 22:07:24 -0700

    Add Rails Lite Part 1 (router and application controller) and intro to Javascript exercises

--- W5D3/skeleton/lib/controller_base.rb
+++ W5D3/skeleton/lib/controller_base.rb
@@ -1,5 +1,6 @@
 require 'active_support'
 require 'active_support/core_ext'
+require 'active_support/inflector'
 require 'erb'
 require_relative './session'
 
@@ -7,34 +8,61 @@ class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res, params={})
+    @req = req
+    @res = res
+    @params = params.merge(req.params)
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    !!@already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise "Multiple responses detected" if already_built_response?
+    @already_built_response = true
+    session.store_session(@res)
+    @res['Location'] = url
+    @res.status = 302
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise "Multiple responses detected" if already_built_response?
+    @already_built_response = true
+    session.store_session(@res)
+    @res['Content-Type'] = content_type
+    @res.write(content)
   end
+
+  # "views/#{controller_name}/#{template_name}.html.erb". Use ActiveSupport's
+  # #underscore (require 'active_support/inflector') method to convert the controller's
+  # class name to snake case.
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    controller_name = self.class.to_s.underscore
+    template_path = "views/#{controller_name}/#{template_name}.html.erb"
+
+    render_file = File.read(template_path)
+    render_erb = ERB.new(render_file)
+    render_string = render_erb.result(binding)
+    render_content(render_string, 'text/html')
   end
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    self.send(name)
+    render(name) unless @already_built_response
   end
 end
-
--- W5D3/skeleton/lib/session.rb
+++ W5D3/skeleton/lib/session.rb
@@ -1,19 +1,34 @@
 require 'json'
+require 'byebug'
 
 class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    cookies = req.cookies['_rails_lite_app']
+    # byebug
+    if cookies
+      @cookie = JSON.parse(cookies)
+    else
+      @cookie = Hash.new
+    end
   end
 
   def [](key)
+    # p 'accessing cookie'
+    @cookie[key]
   end
 
   def []=(key, val)
+    # p 'writing cookie ivar'
+    @cookie[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    return if @cookie == {}
+    cookie_attributes = { path: '/', value: @cookie.to_json }
+    res.set_cookie('_rails_lite_app', cookie_attributes)
   end
 end
